c13ad34fd45e15605cbc9acf11f1fd62fb3a3511
Javier Casares <javier@casares.org>
2023-03-19 09:39:33 +0100

    set chart.js in local folder

--- inc/App/AdminScreen.php
+++ inc/App/AdminScreen.php
@@ -23,9 +23,9 @@ class AdminScreen
 
         wp_enqueue_style('mdbhc--styles', mdbhc_url('css/styles.css'));
 
-        wp_enqueue_script('mdbhc--chartjs', '//cdn.jsdelivr.net/npm/chart.js', array(), null, true);
-        wp_enqueue_script('mdbhc--scripts', mdbhc_url('js/scripts.js'), array(), false, true);
+        wp_enqueue_script('mdbhc--chartjs', mdbhc_url('js/chart.4.2.1.min.js'), array(), null, true);
         wp_enqueue_script('mdbhc--alpinejs', mdbhc_url('js/alpine.3.12.0.min.js'), array(), null, true);
+        wp_enqueue_script('mdbhc--scripts', mdbhc_url('js/scripts.js'), array(), false, true);
     }
 
     public function admin_menu()
