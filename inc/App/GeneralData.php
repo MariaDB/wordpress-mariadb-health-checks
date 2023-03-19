dcaba79393d072e3dd0c49cda9d65538994a7438
Javier Casares <javier@casares.org>
2023-03-19 12:12:31 +0100

    Class improved and more data

--- inc/App/GeneralData.php
+++ inc/App/GeneralData.php
@@ -8,41 +8,200 @@ namespace MDBHC;
 
 class GeneralData {
 
-  public function __construct() {
+    public function __construct() {
 
-    mdbhc_enable_errors();
+        mdbhc_enable_errors();
 
-  }
-
-  public static function get() {
-
-    $generaldata = array();
-    global $wpdb;
-
-    $charset_collate = $wpdb->get_charset_collate();
-
-    $vars = array(
-      '%%VAR_PREFIX%%'    => $wpdb->prefix,
-      '%%VAR_CHARACTER%%' => $charset_collate,
-    );
-
-    $tmp = $wpdb->get_results( "SHOW variables LIKE 'innodb_version'" );
-    if( isset( $tmp[0]->Value ) ) {
-        $generaldata['innodb_version'] = wp_kses( $tmp[0]->Value, 'strip' );
     }
 
+    public static function get() {
 
-    /*
-    $generaldata['protocol_version'] = $wpdb->get_var( "show variables = 'protocol_version'" );
-    $generaldata['tls_version'] = $wpdb->get_var( "show variables = 'tls_version'" );
-    $generaldata['version'] = $wpdb->get_var( "show variables = 'version'" );
-    $generaldata['version_comment'] = $wpdb->get_var( "show variables = 'version_comment'" );
-    $generaldata['version_compile_machine'] = $wpdb->get_var( "show variables = 'version_compile_machine'" );
-    $generaldata['version_compile_os'] = $wpdb->get_var( "show variables = 'version_compile_os'" );
+        $generaldata = array();
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $vars = array(
+            '%%VAR_PREFIX%%' => $wpdb->prefix,
+            '%%VAR_CHARACTER%%' => $charset_collate,
+        );
+
+        $variables = $wpdb->get_results( "SHOW variables" );
+
+        foreach( $variables as $variable ) {
+
+            // Autocommit
+            if( 'autocommit' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['autocommit'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Bind Address
+            if( 'bind_address' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['bind_address'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set Client
+            if( 'character_set_client' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_client'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set Connection
+            if( 'character_set_connection' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_connection'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set Database
+            if( 'character_set_database' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_database'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set Filesystem
+            if( 'character_set_filesystem' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_filesystem'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set Results
+            if( 'character_set_results' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_results'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set Server
+            if( 'character_set_server' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_server'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Character set System
+            if( 'character_set_system' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['character_set_system'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Collation Connection
+            if( 'collation_connection' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['collation_connection'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Collation Database
+            if( 'collation_database' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['collation_database'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Collation Server
+            if( 'collation_server' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['collation_server'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Concurrent insert
+            if( 'concurrent_insert' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['concurrent_insert'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // Concurrent insert
+            if( 'connect_timeout' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['connect_timeout'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+            // date format
+            if( 'date_format' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['date_format'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+    /*        
+            
+            
+            
+            
+            
+            
+            
+            datetime_format
+            
+            error_count
+            
+            expire_logs_days
+            
+            have_compress
+            have_crypt
+            have_dynamic_loading
+            have_geometry
+            have_openssl
+            have_profiling
+            have_query_cache
+            have_rtree_keys
+            have_ssl
+            have_symlink
+            
+            histogram_size
+            histogram_type
+            
+            hostname
+
+            // InnoDB version
+            if( 'innodb_version' == $variable->Variable_name ) {
+                if( isset( $variable->Value ) ) {
+                    $generaldata['innodb_version'] = wp_kses( $variable->Value, 'strip' );
+                }
+            }
+
+            lc_messages
+            lc_time_names
+            
+            license
+            
+            log_error
+            
+            max_connections
+            max_connect_errors
+            max_error_count
+            max_user_connections
+            
+            port
+            protocol_version
+            read_only
+            require_secure_transport
+            server_id
+            slow_query_log
+            slow_query_log_file
+            
+            sql_mode
+            storage_engine
+            
+            time_format
+            time_zone
+            timestamp
+            
+            tls_version
+            
+            version
+            version_comment
+            version_compile_machine
+            version_compile_os
+            version_malloc_library
+            version_source_revision
+            version_ssl_library
+            
+            wait_timeout
+            
+            warning_count
+            
     */
-    return $generaldata;
-  
-  }
-  
-}
+        }
+        return $generaldata;
 
+    }
+
+}
--- templates/admin/main-general.php
+++ templates/admin/main-general.php
@@ -1,19 +1,263 @@
 <h3><?php _e( 'MariaDB Database Information', 'mdbhc' ); ?></h3>
 <?php
-
-$GeneralData = new MDBHC\GeneralData();
-
-$gd = $GeneralData->get();
-
-echo '<pre>';
-print_r( $gd );
-echo '</pre>';
-
-
-if( isset( $gd['innodb_version'] ) ) {
+$mdbhc_GeneralData = new MDBHC\GeneralData();
+$mdbhc_gd = $mdbhc_GeneralData->get();
 ?>
-<p><?php _e( 'InnoDB version', 'mdbhc' ); ?>: <?php echo $gd['innodb_version']; ?></p>
+<h4><?php _e( 'General information', 'mdbhc' ); ?></h4>
+<table style="min-width: 320px;">
+<?php
+$mdbhc_gd_general = true;
+if( isset( $mdbhc_gd['bind_address'] ) ) {
+	$mdbhc_gd_general = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Bind Address', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['bind_address']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['connect_timeout'] ) ) {
+	$mdbhc_gd_general = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Bind Address', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['connect_timeout']; ?></td>
+	</tr>
+<?php
+}
+
+if( isset( $mdbhc_gd['autocommit'] ) ) {
+	$mdbhc_gd_general = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Auto Commit', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['autocommit']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['concurrent_insert'] ) ) {
+	$mdbhc_gd_general = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Concurrent Insert', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['concurrent_insert']; ?></td>
+	</tr>
 <?php
 }
 ?>
+</table>
+<?php
+if( $mdbhc_gd_general ) {
+	_e( 'There is no General information to show.', 'mdbhc' );
+}
+unset( $mdbhc_gd_general );
+?>
 
+<h4><?php _e( 'Date and Time', 'mdbhc' ); ?></h4>
+<table style="min-width: 320px;">
+<?php
+$mdbhc_gd_datetime = true;
+if( isset( $mdbhc_gd['date_format'] ) ) {
+	$mdbhc_gd_datetime = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Date format', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['date_format']; ?></td>
+	</tr>
+<?php
+}
+?>
+</table>
+<?php
+if( $mdbhc_gd_datetime ) {
+	_e( 'There is no Date / Time information to show.', 'mdbhc' );
+}
+unset( $mdbhc_gd_datetime );
+?>
+
+<h4><?php _e( 'Character Set', 'mdbhc' ); ?></h4>
+<table style="min-width: 320px;">
+<?php
+$mdbhc_gd_characterset = true;
+if( isset( $mdbhc_gd['character_set_client'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Client', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_client']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['character_set_connection'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Connection', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_connection']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['character_set_database'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Database', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_database']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['character_set_filesystem'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Filesystem', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_filesystem']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['character_set_results'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Reults', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_results']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['character_set_server'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'Server', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_server']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['character_set_system'] ) ) {
+	$mdbhc_gd_characterset = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: *** Please, some context here *** */
+			_e( 'System', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['character_set_system']; ?></td>
+	</tr>
+<?php
+}
+?>
+</table>
+<?php
+if( $mdbhc_gd_characterset ) {
+	_e( 'There is no Character Set information to show.', 'mdbhc' );
+}
+unset( $mdbhc_gd_characterset );
+?>
+
+<h4><?php _e( 'Character Collation', 'mdbhc' ); ?></h4>
+<table style="min-width: 320px;">
+<?php
+$mdbhc_gd_collation = true;
+if( isset( $mdbhc_gd['collation_connection'] ) ) {
+	$mdbhc_gd_collation = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Connection', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['collation_connection']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['collation_database'] ) ) {
+	$mdbhc_gd_collation = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Database', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['collation_database']; ?></td>
+	</tr>
+<?php
+}
+if( isset( $mdbhc_gd['collation_server'] ) ) {
+	$mdbhc_gd_collation = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Server', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['collation_server']; ?></td>
+	</tr>
+<?php
+}
+?>
+</table>
+<?php
+if( $mdbhc_gd_collation ) {
+	_e( 'There is no Character Collation information to show.', 'mdbhc' );
+}
+unset( $mdbhc_gd_collation );
+?>
+
+<h4><?php _e( 'InnoDB', 'mdbhc' ); ?></h4>
+<table style="min-width: 320px;">
+<?php
+$mdbhc_gd_innodb = true;
+if( isset( $mdbhc_gd['innodb_version'] ) ) {
+	$mdbhc_gd_innodb = false;
+?>
+	<tr>
+		<td><?php
+			/* translators: IP address for the database */
+			_e( 'Version', 'mdbhc' );
+		?></td>
+		<td><?php echo $mdbhc_gd['innodb_version']; ?></td>
+	</tr>
+<?php
+}
+?>
+</table>
+<?php
+if( $mdbhc_gd_innodb ) {
+	_e( 'There is no InnoDB information to show.', 'mdbhc' );
+}
+unset( $mdbhc_gd_innodb );
+
+unset( $mdbhc_gd );
