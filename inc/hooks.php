69abcb76c7047b375819fd607961e8f2553f721f
Alexander Gerber <alex.gerber90@gmail.com>
2023-03-19 18:48:19 +0100

    add site health tests

--- inc/hooks.php
+++ inc/hooks.php
@@ -41,3 +41,46 @@ function mdbhc_save_average_query_execution_time()
 
 add_action('admin_footer', 'mdbhc_save_average_query_execution_time');
 add_action('wp_print_footer_scripts', 'mdbhc_save_average_query_execution_time');
+
+
+function histograms_test() {
+	$result = array(
+		'status'      => 'good',
+		'label'       => 'Histogram Test',
+		'badge'       => array(
+			'label' =>  __( 'Performance' ),
+			'color' => 'blue',
+		),
+		'description' => 'Histograms are up to date',
+		'actions'     => '',
+		'test'        => 'histograms_test',
+	);
+
+	$histograms = new MDBHC\Histograms();
+
+	if($histograms->check() == 0) {
+		$result['status'] = 'recommended';
+		$result['description'] = 'Histograms have not been run!';
+	}
+
+	if($histograms->isReRunNeeded()) {
+		$result['status'] = 'recommended';
+		$result['description'] = 'Rerun is needed.';
+	}
+
+	return $result;
+}
+
+function add_custom_test( $tests ) {
+	$histograms = new MDBHC\Histograms();
+	if($histograms->hasHistograms() != 0) {
+		$tests['direct']['histograms_test'] = [
+			'label' => 'Maria DB Histogram',
+			'test' => 'histograms_test'
+		];
+	}
+
+	return $tests;
+}
+
+add_filter( 'site_status_tests', 'add_custom_test' );
